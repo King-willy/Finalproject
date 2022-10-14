7c5ac2b5fa1ffc4089607edbc1f7bd972e7f8aa9
King-willy <william.kaiganira@student.moringaschool.com>
2022-10-14 15:35:06 +0300

    edit scripts

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ menu.onclick = () =>{
   navbar.classList.toggle('active');
 }
 
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
 
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  };
+
+};
 
